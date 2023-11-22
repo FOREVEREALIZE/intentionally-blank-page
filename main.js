aa46589dc91ed5553fa343ae72d2fae9868bcf5a
FOREVEREALIZE <felixcrat@gmail.com>
2023-11-22 16:59:37 +0000

    Add Text Changer

--- main.js
+++ main.js
@@ -8,12 +8,33 @@ $(document).ready(() => {
         const targetClass = isDark ? 'light' : 'dark'
         const targetIcon = isDark ? 'sun' : 'moon'
         
-        console.log('[THEME CHANGE] Change from ' + originClass.toUpperCase() + ' to ' + targetClass.toUpperCase())
+        console.log('[THEME CHANGE] Transition begin.')
 
-        $('.' + originClass).addClass(targetClass).removeClass(originClass)
+        $('.theme-affected').toggleClass("light dark")
+        $("#popup-container").toggleClass("pc-light pc-dark")
+        $("#close-button").toggleClass("cb-light cb-dark")
+        $("#popup").toggleClass("p-light p-dark")
         $('#theme-button').html('<i data-feather="' + targetIcon + '" class="feather"></i>')
         
         feather.replace()
         isDark = !isDark
+
+        console.log('[THEME CHANGE] Transition end.')
+    })
+
+    $('#type-button').click(() => {
+        console.log("[TEXT POPUP] Transition start.")
+
+        $("#popup-container").toggleClass("pc-hide pc-show")
+    })
+
+    $('#close-button').click(() => {
+        console.log("[TEXT POPUP] Transition start.")
+
+        $("#popup-container").toggleClass("pc-hide pc-show")
+    })
+
+    $("#textinput").on("input", (e) => {
+        $("#bigtext").text(e.target.value)
     })
 })
